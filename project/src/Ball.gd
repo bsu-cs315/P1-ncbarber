3f4a3835bebcbebd2667c95d647409111d929155
Nathaniel Barber` <ncbarber@bsu.edu>
2021-09-02 12:54:12 -0400

    Add up-arrow logic to increase angle

--- project/src/Ball.gd
+++ project/src/Ball.gd
@@ -1,8 +1,11 @@
 extends RigidBody2D
 
-var angle = -45
-var impulse = Vector2.RIGHT.rotated(deg2rad(angle)) * 250
+var angle = 0
 
-func _process(_delta):
+func _process(delta):
 	if Input.is_action_just_pressed("launch"):
+		var impulse = Vector2.RIGHT.rotated(deg2rad(angle)) * 250
 		apply_impulse(Vector2.ZERO, impulse)
+	if Input.is_action_pressed("increase angle"):
+		angle -= 20 * delta
+		print(angle)
