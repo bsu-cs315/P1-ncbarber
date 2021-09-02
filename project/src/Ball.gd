7741a74a05542060f8dc52e9eaad74fe218eb4d0
Nathaniel Barber` <ncbarber@bsu.edu>
2021-09-02 13:02:52 -0400

    Allow decreasing angle

--- project/src/Ball.gd
+++ project/src/Ball.gd
@@ -1,11 +1,15 @@
 extends RigidBody2D
 
 var angle = 0
+export var degrees_per_second = 20
 
 func _process(delta):
 	if Input.is_action_just_pressed("launch"):
 		var impulse = Vector2.RIGHT.rotated(deg2rad(angle)) * 250
 		apply_impulse(Vector2.ZERO, impulse)
 	if Input.is_action_pressed("increase angle"):
-		angle -= 20 * delta
+		angle -= degrees_per_second * delta
+		print(angle)
+	if Input.is_action_pressed("decrease angle"):
+		angle += degrees_per_second * delta
 		print(angle)
