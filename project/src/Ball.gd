951c457e7d3c5d3a9c25891cecb7a364677198f0
Nathaniel Barber` <ncbarber@bsu.edu>
2021-09-06 10:14:55 -0400

    Added beginnings of launching power

--- project/src/Ball.gd
+++ project/src/Ball.gd
@@ -1,15 +1,21 @@
 extends RigidBody2D
 
-var angle = 0
+var _angle = 0
 export var degrees_per_second = 20
+export var power = 200
+export var speed = 100
 
 func _process(delta):
 	if Input.is_action_just_pressed("launch"):
-		var impulse = Vector2.RIGHT.rotated(deg2rad(angle)) * 250
+		var impulse = Vector2.RIGHT.rotated(deg2rad(_angle)) * 250
 		apply_impulse(Vector2.ZERO, impulse)
 	if Input.is_action_pressed("increase angle"):
-		angle -= degrees_per_second * delta
-		print(angle)
+		_angle -= degrees_per_second * delta
+		print(_angle)
 	if Input.is_action_pressed("decrease angle"):
-		angle += degrees_per_second * delta
-		print(angle)
+		_angle += degrees_per_second * delta
+		print(_angle)
+	if Input.is_action_pressed("increase power"):
+		speed += power* delta
+	if Input.is_action_pressed("decrease power"):
+		speed -= power * delta
