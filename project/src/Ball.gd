e5f62ffb16b26185a817011ae21450112b59e930
Nathaniel Barber` <ncbarber@bsu.edu>
2021-09-06 16:39:17 -0400

    Revert "Setting up arrow, and arrow movement with clamps"
    This reverts commit 033a2c7524ebacf0c68b7a0196ef8fed97751bc8.

--- project/src/Ball.gd
+++ project/src/Ball.gd
@@ -3,32 +3,22 @@ extends RigidBody2D
 var _angle = 0
 export var degrees_per_second = 20
 export var power = 200
-export var exit_speed = 0
+export var exit_speed = 100
 
 func _process(delta):
 	if Input.is_action_just_pressed("launch"):
 		var exit_angle = Vector2.RIGHT.rotated(deg2rad(_angle))
 		var impulse = exit_angle * exit_speed
 		apply_impulse(Vector2.ZERO, impulse)
-		$ArrowRotation.visible = false
 	if Input.is_action_pressed("increase angle"):
 		_angle -= degrees_per_second * delta
-		_angle = clamp(_angle, -90, 0)
-		#print(_angle)
-		update_arrow_angle(_angle)
+		print(_angle)
 	if Input.is_action_pressed("decrease angle"):
 		_angle += degrees_per_second * delta
-		_angle = clamp(_angle, -90, 0)
-		#print(_angle)
-		update_arrow_angle(_angle)
+		print(_angle)
 	if Input.is_action_pressed("increase power"):
 		exit_speed += power* delta
-		exit_speed = clamp(exit_speed, 0, 900)
 		print(exit_speed)
 	if Input.is_action_pressed("decrease power"):
 		exit_speed -= power * delta
-		exit_speed = clamp(exit_speed, 0, 900)
 		print(exit_speed)
-
-func update_arrow_angle(exit_angle):
-	$ArrowRotation.rotation_degrees = exit_angle
