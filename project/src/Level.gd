30122c2b0f5b9e006d3ec34800607bc50182a257
Nathaniel Barber` <ncbarber@bsu.edu>
2021-09-12 16:43:53 -0400

    Edit Stat functionality, add Restart button 'R'

--- project/src/Level.gd
+++ project/src/Level.gd
@@ -1,3 +1,13 @@
 extends Node2D
 
 
+func _process(_delta):
+	
+	if Input.is_action_just_pressed("restart"):
+		restart()
+
+func _on_Star_Target_entered(_body):
+	print("Hit!")
+
+func restart():
+	var _restart_game = get_tree().change_scene("res://src/Level.tscn")
--- project/src/Star_Target.gd
+++
@@ -1,5 +0,0 @@
-extends Area2D
-
-
-func _on_Star_Target_body_entered(_body):
-	print("collision")
