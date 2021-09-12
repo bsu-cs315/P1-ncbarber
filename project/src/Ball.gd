1eadbe55cb01ab8393c32f6d25a70f0770bc0223
Nathaniel Barber` <ncbarber@bsu.edu>
2021-09-12 16:27:06 -0400

    Added Font, Labels, and Update Star to Area2D

--- project/src/Ball.gd
+++ project/src/Ball.gd
@@ -1,37 +1,47 @@
 extends RigidBody2D
 
+signal angle_changed(new_angle)
+signal power_changed(new_power)
+
 var _angle = 0
 export var degrees_per_second = 20
-export var power = 200
-export var exit_speed = 100
+export var _power = 200
+export var _exit_speed = 100
 
 func _process(delta):
 	if Input.is_action_just_pressed("launch"):
 		var exit_angle = Vector2.RIGHT.rotated(deg2rad(_angle))
-		var impulse = exit_angle * exit_speed
+		var impulse = exit_angle * _exit_speed
 		apply_impulse(Vector2.ZERO, impulse)
 		$ArrowRotation.visible = false
+		
+		
 	if Input.is_action_pressed("increase angle"):
 		_angle -= degrees_per_second * delta
 		_angle = clamp(_angle, -90, 0)
-		#print(_angle)
+		emit_signal("angle_changed", _angle)
 		update_arrow_angle(_angle)
+		
+		
 	if Input.is_action_pressed("decrease angle"):
 		_angle += degrees_per_second * delta
 		_angle = clamp(_angle, -90, 0)
-		#print(_angle)
+		emit_signal("angle_changed", _angle)
 		update_arrow_angle(_angle)
+		
+		
 	if Input.is_action_pressed("increase power"):
-		exit_speed += power* delta
-		exit_speed = clamp(exit_speed, 0, 900)
-		#print(exit_speed)
-		update_arrow_power(exit_speed)
+		_exit_speed += _power* delta
+		_exit_speed = clamp(_exit_speed, 0, 900)
+		emit_signal("power_changed", _power)
+		update_arrow_power(_exit_speed)
+		
+		
 	if Input.is_action_pressed("decrease power"):
-		exit_speed -= power * delta
-		exit_speed = clamp(exit_speed, 0, 900)
-		#print(exit_speed)
-		update_arrow_power(exit_speed)
-
+		_exit_speed -= _power * delta
+		_exit_speed = clamp(_exit_speed, 0, 900)
+		emit_signal("power_changed", _power)
+		update_arrow_power(_exit_speed)
 		
 func update_arrow_angle(exit_angle):
 	$ArrowRotation.rotation_degrees = exit_angle
--- project/src/Star_Target.gd
+++ project/src/Star_Target.gd
@@ -1,4 +1,4 @@
-extends RigidBody2D
+extends Area2D
 
 
 func _on_Star_Target_body_entered(_body):
